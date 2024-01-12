2fd535a1e2fe40120cda9fa96db89a6c41da35a9
felipesanma <felipesmsoto@gmail.com>
2024-01-12 17:43:10 -0300

    adding winner func

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import { useState } from 'react';
 
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   console.log(squares);
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+    console.log(nextSquares);
+    const isGameOver = calculateWinner(nextSquares);
+    console.log(isGameOver);
   }
 
   return (
@@ -38,4 +50,27 @@ function Square({ value, onSquareClick }) {
       {value}
     </button>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    console.log(lines[i]);
+    console.log(squares[a], squares[b], squares[c]);
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+
 }
