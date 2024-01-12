39ad443ee06d54a1440bcdec61bded736f5ced3f
felipesanma <felipesmsoto@gmail.com>
2024-01-12 14:40:09 -0300

    clening

--- src/App.js
+++ src/App.js
@@ -1,49 +1,8 @@
-import { useState } from 'react';
-
-
-const products = [
-  { title: 'Cabbage', isFruit: false, id: 1 },
-  { title: 'Garlic', isFruit: false, id: 2 },
-  { title: 'Apple', isFruit: true, id: 3 },
-];
-
-export default function MyApp() {
+export default function Square() {
   return (
-    <div>
-        <ShoppingList />
-        <MyButton />
-        <MyButton /> 
-        </div>
-  );
-}
-
-export function ShoppingList() {
-  const listItems = products.map(product =>
-    <li
-      key={product.id}
-      style={{
-        color: product.isFruit ? 'blue' : 'darkgreen'
-      }}
-    >
-      {product.title}
-    </li>
-  );
-
-  return (
-    <ul>{listItems}</ul>
-  );
-}
-
-export function MyButton() {
-  const [count, setCount] = useState(0);
-
-  function handleClick() {
-    setCount(count + 1);
-  }
-
-  return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
   );
 }
